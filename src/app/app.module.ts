6a1b0a297a5ca1a595a5958bfa3c17428a99f7f6
Evandro Vieira de Carvalho Junior <evandrovcjunior@gmail.com>
2018-02-25 22:33:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DataTableModule } from 'primeng/primeng';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { ItemService } from './item.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ItemService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/item-cadastro/item-cadastro.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ItemService } from '../item.service';
+
+
+@Component({
+  selector: 'app-item-cadastro',
+  templateUrl: './item-cadastro.component.html',
+  styleUrls: ['./item-cadastro.component.css']
+})
+export class ItemCadastroComponent implements OnInit {
+
+  itens = [];
+
+  constructor( private itemService : ItemService ) { }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.itemService.listar()
+      .subscribe( (dados) => this.itens = dados )
+  }
+}
+
